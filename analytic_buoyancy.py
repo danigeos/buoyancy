2f01eabfabe9efead860d02851f88e3198f90275
Daniel Garcia-Castellanos <danielgc@ictja.csic.es>
2018-02-09 16:30:35 +0100

    simplifying and commenting

--- analytic_buoyancy.py
+++ analytic_buoyancy.py
@@ -14,25 +14,22 @@ Mytosec=1e6*365.25*24*3600
 
 
 #-------------------------------------
-#vel=3. #km/Myr
-#Compositional density contrast at the LAB
-Drho_comp=10. #12, 33, 50 (archon), 64 kg m-3 Contrast between base lithosphere and top asthenosphere
+#Compositional density contrast at the LAB (kg m-3):
+Drho_comp=41. #12 (100km tecton), 25 (200km proton), 33 (100km proton), 64 (100km archon), 68 (200km archon)
 vel=10 #mm/yr
+drho_dT=.1175 #Density dependence with temperature, kg m-3 K-1
+zm=5e3 #Moho depth (top pf plate), m
+zl=20e3 #Base of the Lithosphere, LAB, m
+Tm=400  #Moho temperature C
+Ta=1300 #Asthenosphere temperature C
+sigma=45. #Subduction angle in degrees
+tmax=50 #Final time in Myr
 
-drho_dT=.1175 #kg m-3 K-1
-zm=25e3  #m
-zl=200e3 #m
-Tm=700  #C
-Ta=1300 #C
-sigma=45. #subduction angle in degrees
-
-#time=5.*Mytosec #Myr
-tmax=50
-H=zl-zm
-Dl=H/4
-bb=H*H/8./alpha
 #-------------------------------------
 
+H=zl-zm 
+Dl=H/4
+bb=H*H/8./alpha
 
 nit=101
 
@@ -65,31 +62,29 @@ for i in range(0, nit):
 	if (time>time1): 
 		timeL=time1
 		print('\nTime longer than Stage 1 (question results for diffusion)')
-	print("i=%d  vel=%.2f km/My    time=%.2f My  timeSt1=%.2f My   Dl=%.1f km" %(i, vel*Mytosec/1e3, time/Mytosec, time1/Mytosec, Dl/1e3))
+	print("i=%d  vel=%.2f km/My    time=%.2f My  Dl=%.1f km" %(i, vel*Mytosec/1e3, time/Mytosec, Dl/1e3))
 	#Critical Drho_comp that is overcome by advection:
 	Drho_comp_crit = 1./2. * drho_dT * (Ta-Tm)
-	print('Drho_comp_crit= %.1f [kg/m3]' %Drho_comp_crit)
 	dFsp_adv_temp_dt_ini = g * Drho_comp_crit * H * vel
 	#dFsp_adv_temp_dt = + dFsp_adv_temp_dt_ini * math.pow((1 - time/time1), 2)
 	#Average density difference between LM and asthenosphere at LAB
-	Drho_comp_avg = Drho_comp - Drho_comp_crit*math.sin(sigma)/H*vel*timeL
-	print('Drho_comp_avg = %.2e' %(Drho_comp_avg))
+	#Drho_comp_avg = Drho_comp - Drho_comp_crit*math.sin(sigma)/H*vel*timeL
+	#print('Drho_comp_avg = %.2e' %(Drho_comp_avg))
 	#dFsp_adv_comp_dt = - Drho_comp_avg * g * H * vel
-	#print('Buoyany_force_per_My= %+.1e + %+.1e = %+.1e [N/m/My]' %((dFsp_adv_temp_dt)*Mytosec, (dFsp_adv_comp_dt)*Mytosec, (dFsp_adv_temp_dt+dFsp_adv_comp_dt)*Mytosec))
+	#print('Buoyant_force_per_My= %+.1e + %+.1e = %+.1e [N/m/My]' %((dFsp_adv_temp_dt)*Mytosec, (dFsp_adv_comp_dt)*Mytosec, (dFsp_adv_temp_dt+dFsp_adv_comp_dt)*Mytosec))
 
 	#Advection above zl:
 	Fby_adv_temp[i] = - dFsp_adv_temp_dt_ini * time1 / 3. * (1-math.pow(1-timeL/time1,3))
 
 	#Advection below zl:
 	#Stage 1:
-	Fby_adv_comp[i] = g*H*Drho_comp*vel*timeL + 1./4*g*drho_dT*(Ta-Tm)*math.sin(sigma) * math.pow(vel*timeL,2)
+	Fby_adv_comp[i] = + g*H*Drho_comp*vel*timeL - 1./2.*g*Drho_comp_crit*math.sin(sigma) * math.pow(vel*timeL,2)
 	#Stage 2:
 	if (time>time1): 
-		Fby_adv_comp[i] += (Drho_comp_crit - Drho_comp) * g * H * vel * (time-time1)
+		Fby_adv_comp[i] += g * (Drho_comp - Drho_comp_crit) * H * vel * (time-time1)
 
-	print('\tBuoyancy by advection of temp. = %+.2e [N/m] (blue)'  %(Fby_adv_temp[i]))
-	print('\tBuoyancy by advection of comp. = %+.2e [N/m] (green)' %(Fby_adv_comp[i]))
-
+	print('\tBuoyancy by advection above zl = %+.2e [N/m] (blue)'  %(Fby_adv_temp[i]))
+	print('\tBuoyancy by advection below zl = %+.2e [N/m] (green)' %(Fby_adv_comp[i]))
 
 	#Diffusion 1st approach
 	#Diffusion lowermost triangle (Stage 1+2):
@@ -112,21 +107,20 @@ for i in range(0, nit):
 	#		- ff * time1 * ( time+bb*explust1*expmint - time1 - bb ) 
 
 	#Diffusion below LAB - 3rd approach (simplest and most accurate!): 
-	ff = 4. * alpha * g * (Ta-Tm) / H * drho_dT * bb * vel
+	#ff = 4. * alpha * g * (Ta-Tm) / H * drho_dT * bb * vel
 	ff = 1./2. * g * drho_dT * (Ta-Tm) * H * vel
-	print('\tFactor = %+.1e' %ff)
-
+	#print('\tFactor = %+.1e' %ff)
 	expmint = math.exp(-time/bb)
 	Fby_dif[i] = ff * ( time + bb*(expmint-1) ) 
 
 	#Diffusion above LAB:
 	#MISSING!!
 
-	print('\tBuoyancy by diffusion of temp. = %+.2e [N/m] (red)' %Fby_dif[i])
+	print('\tBuoyancy by diffusion below zl = %+.2e [N/m] (red)' %Fby_dif[i])
 
 
 	Fby_total[i]=Fby_adv_temp[i]+Fby_adv_comp[i]+Fby_dif[i]
-	print('\tTOTAL buoyancy force      = %+.1e [N/m] (GREY)' %Fby_total[i])
+	print('\tTOTAL buoyant force (>0 =>upwards) = %+.1e [N/m] (GREY)' %Fby_total[i])
 
 plt.cla()
 plt.hlines(0, 0, tmax, 'k', 'dotted', '')
@@ -136,7 +130,7 @@ plt.plot(time_arr,Fby_adv_temp, 'b', linewidth=1.5)
 plt.plot(time_arr,Fby_adv_comp, 'g', linewidth=1.5)
 plt.plot(time_arr,Fby_dif, 'r', linewidth=1.5)
 plt.plot(time_arr,Fby_total, 'grey', linewidth=3)
-plt.ylim([-6e12,15e12])
+plt.ylim([-3e11,5e11])
 #plt.xlabel("vel (km/My)")
 plt.xlabel("time (My)")
 plt.ylabel("buoyancy (N/m)")
@@ -147,5 +141,7 @@ line_4, = plt.plot([3,2,1], 'gray', label='Line 1')
 plt.legend([line_1, line_2, line_3, line_4], ['Advection_above_LAB', 'Advection_below_LAB', 'Diffusion', 'Total=Adv+Diff'], loc=2)
 plt.show(block=False)
 plt.savefig('analytic_buoyancy.ps', format='ps', dpi=1000)
+print('Drho_comp_crit= %.1f [kg/m3]' %Drho_comp_crit)
+print('timeSt1=%.2f My' %(time1/Mytosec))
 input("Press Enter to continue...")
 #plt.close()
